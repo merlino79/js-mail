76879ba2a97aa04e4b81aebdacf29ea9ffbee97f
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-10 14:33:29 +0200

    controllo lista

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-var nomi = [
-    "luca",
-    "marco",
-    "serena ",
-    "tey",
-    "gianluca",
-    "alessandro",
-    "simone",
-    "martino",
-    "silvia"
+//creazione lista 
+var email = [
+    "luca@gmail.com",
+    "marco@libero.it",
+    "serena@libero.it ",
+    "tey@libero.it",
+    "gianluca@yahoo.it",
+    "alessandro@hotmail.com",
+    "simone@gmail.com",
+    "martino@gmail.com",
+    "silvia@gmail.com"
 ];
+
+console.log(email.length - 1);
+
+//controllo della lista
